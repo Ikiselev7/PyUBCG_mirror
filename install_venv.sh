f5d1d44fcccf0ae05f67e83267ff7119fb8c2dec
Ilia Kiselev <ilia_kiselev@epam.com>
2019-01-28 22:30:27 +0300

    Fix install_venv.sh

--- install_venv.sh
+++ install_venv.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 virtualenv -q -p `which python3` pyubcg_venv
-PyUBCG/bin/pip install -r requirements.txt
+pyubcg_venv/bin/pip install -r requirements.txt
 
 echo "#!/usr/bin/env bash" > venv.sh
-echo "source PyUBCG/bin/activate" >> venv.sh
+echo "source pyubcg_venv/bin/activate" >> venv.sh
 chmod +x venv.sh
