82a090fec1cf06ec82f185e6ab4bd638b972bf83
Ilia Kiselev <Ilia_Kiselev@epam.com>
2019-04-02 22:37:25 +0300

    PyUBCG-070: multiple extract logging added

--- PyUBCG/app.py
+++ PyUBCG/app.py
@@ -192,6 +192,7 @@ class Main:
         self._config.format = None
         self._config.type = None
         for fasta in fastas:
+            LOGGER.info("Processing: " + fasta)
             name = fasta.split('/')[-1].rsplit('.', 1)[0]
             self._config.label = name
             self.extract(fasta)
